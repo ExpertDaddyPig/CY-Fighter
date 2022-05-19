c6acc4f8a8c6fdf621fd85ec603781d14f44717d
De Lacoste De Laval Clement <delacosted@pc-cy216-023.cpi.cergy.eisti.fr>
2022-05-19 12:51:08 +0200

    Cool

---
+++ combatFunctions.h
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "baseFunctions.h"
+#include "baseFunctions.c"
+#include "main.c"
+
+void Interface(){
+	int space;
+	printf("_[%s]",teamName);
+	space=strlen(teamName);
+	for(int i=0; i<space;i++){
+		
+	}
+	
+}
+
+int Tours(){
+	int n;
+	do{
+	printf("Début de la manche %d",n);
+	
+	n=n+1;
+	}
+}
+
+void main(){
+	printf("Le joueur 1 à choisi %s, %s et %s !",getFighter(heros1),getFighter(heros2),getFighter(heros3));
+	
+	
+}
+
+┏[EQUIPE1]━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃    sasuke  |1|              sakura  |¤|              naruto  |3|            ┃
+┃    (***)                                                                    ┃
+┃   [#################   ]   [#################   ]   [##############      ]  ┃
+┃   [>>>>>>              ]   [>>>                 ]   [>>>>>>>>>>>>>>>>>>  ]  ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+┏[EQUIPE2]━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃    kiba                     shino                    )>hinata<(             ┃
+┃    (***)                                                                    ┃
+┃   [#################   ]   [#################   ]   [##############      ]  ┃
+┃   [>>>>>>              ]   [>>>                 ]   [>>>>>>>>>>>>>>>>>>  ]  ┃
+┃                                                                             ┃
+┃     hinata                                                                  ┃
+┃     techniques speciales                                                    ┃
+┃         |1| Double Kunaï (durée: 3 tours)                                   ┃
+┃              ¤ Permet de frapper 2 fois par tour                            ┃
+┃         |-| Ecran de fumée (recharge: 2/durée: 4 tours)                     ┃
+┃              ¤ Permet d'augmenter l'esquive de 75%                          ┃
+┃         |3| Barrière de glace (durée: 3 tours)                              ┃
+┃              ¤ Permet d'augmenter la défense de 50%                         ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
