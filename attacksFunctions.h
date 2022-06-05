73a8ffcc0960246905ffb2e24a057fd0f3d0b767
Expert Daddy Pig <67962204+ExpertDaddyPig@users.noreply.github.com>
2022-06-06 01:12:58 +0200

    ATTAQUES

---
+++ attacksFunctions.h
@@ -0,0 +1,45 @@
+#ifndef ATTACKSFUNCTIONS_H
+#define ATTACKSFUNCTIONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "baseFunctions.h"
+
+void basic(ActiveTeam *fighter, Team *ally, Team *enemy);
+void pizza(ActiveTeam *fighter, Team *ally, Team *enemy);
+void avion(ActiveTeam *fighter, Team *ally, Team *enemy);
+void kamehameha(ActiveTeam *fighter, Team *ally, Team *enemy);
+void kaioken(ActiveTeam *fighter, Team *ally, Team *enemy);
+void ramen(ActiveTeam *fighter, Team *ally, Team *enemy);
+void paume(ActiveTeam *fighter, Team *ally, Team *enemy);
+void spiked(ActiveTeam *fighter, Team *ally, Team *enemy);
+void acier(ActiveTeam *fighter, Team *ally, Team *enemy);
+void seisme(ActiveTeam *fighter, Team *ally, Team *enemy);
+void thunder(ActiveTeam *fighter, Team *ally, Team *enemy, int pos1, int pos2);
+void fire(ActiveTeam *fighter, Team *ally, Team *enemy);
+void boomerang(ActiveTeam *fighter, Team *ally, Team *enemy);
+void ice(ActiveTeam *fighter, Team *ally, Team *enemy);
+void rasengan(ActiveTeam *fighter, Team *ally, Team *enemy);
+void clone(ActiveTeam *fighter, Team *ally, Team *enemy);
+void golf(ActiveTeam *fighter, Team *ally, Team *enemy);
+void peche(ActiveTeam *fighter, Team *ally, Team *enemy);
+void boule(ActiveTeam *fighter, Team *ally, Team *enemy);
+void punch(ActiveTeam *fighter, Team *ally, Team *enemy);
+void cherry(ActiveTeam *fighter, Team *ally, Team *enemy);
+void heal(ActiveTeam *fighter, Team *ally, Team *enemy);
+void katon(ActiveTeam *fighter, Team *ally, Team *enemy);
+void chidori(ActiveTeam *fighter, Team *ally, Team *enemy);
+void croque(ActiveTeam *fighter, Team *ally, Team *enemy);
+void repos(ActiveTeam *fighter, Team *ally, Team *enemy);
+void mysteres(ActiveTeam *fighter, Team *ally, Team *enemy);
+void spinattack(ActiveTeam *fighter, Team *ally, Team *enemy);
+void homming(ActiveTeam *fighter, Team *ally, Team *enemy);
+void bots(ActiveTeam *fighter, Team *ally, Team *enemy);
+void hand(ActiveTeam *fighter, Team *ally, Team *enemy);
+void flash(ActiveTeam *fighter, Team *ally, Team *enemy);
+void garrick(ActiveTeam *fighter, Team *ally, Team *enemy);
+
+
+
+#endif
